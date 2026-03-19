bd459cd022b9966b4dd5c0948e2d1eaa72cb2ea3
Pierre-André Docquin <pierreandre.docquin@gmail.com>
2026-03-19 18:12:17 +0100

    Add Sprint 2 files

---
+++ infrastructure/localisation-db/init/01-extensions.sql
@@ -0,0 +1,10 @@
+DO $$
+BEGIN
+	IF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'timescaledb') THEN
+		CREATE EXTENSION IF NOT EXISTS timescaledb;
+	END IF;
+
+	IF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'postgis') THEN
+		CREATE EXTENSION IF NOT EXISTS postgis;
+	END IF;
+END $$;
